b977d65bf901bf941081dc09b7bfe88c856e6eb8
petrykivd <petrykiv.dmytro19@gmail.com>
2024-03-21 17:03:27 +0200

    created send_message endpoint

--- main.py
+++ main.py
@@ -1,13 +1,31 @@
-from fastapi import FastAPI
+import requests
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
 
 app = FastAPI()
 
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+class Message(BaseModel):
+    bot_token: str
+    chat_id: str
+    message: str
 
 
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
+@app.post("/send_message")
+async def send_message(message: Message):
+    bot_token = message.bot_token
+    chat_id = message.chat_id
+    message_text = message.message
+
+    telegram_url = f"https://api.telegram.org/bot{bot_token}/sendMessage"
+    payload = {
+        "chat_id": chat_id,
+        "text": message_text
+    }
+
+    response = requests.post(telegram_url, json=payload)
+
+    if response.ok:
+        return {"status": "success", "message": "Message sent successfully"}
+    else:
+        raise HTTPException(status_code=response.status_code, detail="Failed to send message")
